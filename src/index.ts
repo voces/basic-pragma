aa0bdfc19e8ac3a6e32d10ac0395337d17e8f07b
Robert Coe <chakrawc@gmail.com>
2020-11-03 08:15:09 -0800

    null handling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 export { Adapter, setAdapter } from "./adapter";
-export { createElement, Fragment, VNode } from "./element";
+export {
+	createElement,
+	Fragment,
+	VNode,
+	Children,
+	EmptyObject,
+} from "./element";
 export { render } from "./reconciler";
 export { useReducer } from "./hooks/useReducer";
 export { useState } from "./hooks/useState";
+export { FunctionalComponent } from "./Component";
---
+++ src/utils/arrays.ts
@@ -0,0 +1,29 @@
+export const getLength = <T>(arr: T[]): number =>
+	Object.keys(arr).reduce((max, key) => {
+		const keyAsNumber = parseInt(key);
+		if (typeof keyAsNumber === "number")
+			return max > keyAsNumber ? max : keyAsNumber;
+		return max;
+	}, 0);
+
+/**
+ * Removes nils from the array
+ */
+export const compact = <T>(arr: (T | undefined | null)[]): T[] => {
+	const length = getLength(arr);
+	const newArr = [];
+	for (let i = 0; i < length; i++) {
+		const val = arr[i];
+		if (val != null) newArr.push(val);
+	}
+	return newArr;
+};
+
+export const forEach = <T>(
+	arr: T[],
+	fn: (element: T, index: number) => void,
+): void => {
+	const length = getLength(arr);
+
+	for (let i = 0; i < length; i++) fn(arr[i], i);
+};
